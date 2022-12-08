571c8f52fd4b2658290e893c991387304a5bc831
hampug1 <98275225+hampug1@users.noreply.github.com>
2022-12-08 18:01:53 +0000

    Add files via upload

---
+++ main_85.js
@@ -0,0 +1,22 @@
+menu_list_array = [ "Veg Margherita Pizza",                  
+  "pepperoni pizza","hawaiian pizza","meatfeast pizza"]; //Add more Pizza item names
+              
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("add_item").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.push(item);
+}
+
+                                         
